84f51aa09b977ac7e798ab0830a6cffb596444b6
tajima2019 <tajima.tajiken@gmail.com>
2026-02-08 15:36:03 +0900

    change sketchybar settings

--- sketchybar.back/plugins/volume.sh
+++ sketchybar.back/plugins/volume.sh
@@ -3,19 +3,17 @@
 # The volume_change event supplies a $INFO variable in which the current volume
 # percentage is passed to the script.
 
-source "$CONFIG_DIR/icons.sh"
-
 if [ "$SENDER" = "volume_change" ]; then
   VOLUME="$INFO"
 
   case "$VOLUME" in
-    [6-9][0-9]|100) ICON=$ICON_VOLUME_100
+    [6-9][0-9]|100) ICON="󰕾"
     ;;
-    [3-5][0-9]) ICON=$ICON_VOLUME_66
+    [3-5][0-9]) ICON="󰖀"
     ;;
-    [1-9]|[1-2][0-9]) ICON=$ICON_VOLUME_33
+    [1-9]|[1-2][0-9]) ICON="󰕿"
     ;;
-    *) ICON=$ICON_VOLUME_0
+    *) ICON="󰖁"
   esac
 
   sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"
