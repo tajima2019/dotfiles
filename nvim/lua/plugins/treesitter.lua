6f4ce894418bfeb380a6f3c359f63f9456db74e6
tajima2019 <tajima.tajiken@gmail.com>
2026-02-12 10:42:31 +0900

    add lazydocker

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,11 +7,42 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter").setup({
-        vim.api.nvim_create_autocmd("FileType", {
-          callback = function()
-            pcall(vim.treesitter.start)
-          end,
-        })
+        highlight = { enable = true },
+        ensure_installed = {
+          "bash",
+          "c",
+          "cpp",
+          "css",
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "rust",
+          "toml",
+          "typescript",
+          "yaml",
+        },
+      })
+
+      -- treesitterを有効化
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
+
+      -- Markdown folding を有効化
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "markdown" },
+        callback = function()
+          vim.opt_local.foldmethod = "expr"
+          vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.opt_local.foldenable = true
+          vim.opt_local.foldlevel = 99
+        end,
       })
     end,
   },
